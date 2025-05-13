a342def132304051d0082b3e1e88adb8538b7fd9
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2025-05-14 00:09:23 +0500

    datasecure.py

--- assignment_05/datasecure.py
+++ assignment_05/datasecure.py
@@ -53,7 +53,7 @@ stored_data = load_data()
 
 # === navigation bar ===
 st.title(" 🔐 Secure Data Encryption System")
-menu = ["Home", "Register", "Login", "Stored_data", "Retrieve Data"]
+menu = ["Home", "Register", "Login", "Store Data", "Retrieve Data"]
 choice = st.sidebar.selectbox("Navigation", menu)
 
 if choice == "Home":
