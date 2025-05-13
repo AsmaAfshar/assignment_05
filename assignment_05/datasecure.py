763d186641fbeff5de70c42c999bf8f2efd5adfa
Asma Afshar <161859558+AsmaAfshar@users.noreply.github.com>
2025-05-14 00:28:48 +0500

    datasecure.py

--- assignment_05/datasecure.py
+++ assignment_05/datasecure.py
@@ -97,7 +97,7 @@ elif choice == "Login":
      username = st.text_input("Username")
      password = st.text_input("Password", type="password")
         
-    if st.button("Login"):
+     if st.button("Login"):
         if username in stored_data and stored_data[username]["password"] == hash_password(password):
             st.session_state.authenticated_user = username
             st.session_state.failed_attempts = 0
